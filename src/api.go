dd55f8cdbdfb3b0b58f571a514914847e85ab289
ramirezg <gabriel.ramirez@gsa.gov>
2015-07-19 14:25:40 -0400

    adding routes that support dates

--- src/api.go
+++ src/api.go
@@ -27,10 +27,14 @@ type Quotas []Quota
 func main() {
 
 	router := httprouter.New()
+	//TODO: Set to be main route
+	//TODO: Add authentication
 	router.GET("/", Index)
 	router.GET("/api/quotas", QuotaIndex)
 	router.GET("/api/quotas/:guid", QuotaDetails)
 	router.ServeFiles("/static/*filepath", http.Dir("static/"))
+	router.ServeFiles("/dist/*filepath", http.Dir("static/dist/"))
+
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
 
@@ -40,6 +44,17 @@ func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 
 func QuotaIndex(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 
+	//TODO: Convert to function
+	query_values := r.URL.Query()
+	since := query_values.Get("since")
+	if since == "" {
+		since = "1970-01-01"
+	}
+	until := query_values.Get("until")
+	if until == "" {
+		until = "2050-01-01"
+	}
+	//TODO: Setup to only open once
 	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
 	if err != nil {
 		fmt.Println(err)
@@ -47,24 +62,52 @@ func QuotaIndex(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	defer db.Close()
 	var quotas string
 	err = db.QueryRow(`
-		SELECT json_agg(t) AS elements FROM (SELECT guid, name, data FROM quotas_view ) t
-	`).Scan(&quotas)
-	if err != nil {
+		with quota_details_agg as (
+		  select * from get_quotas_details($1, $2)
+		)
+		SELECT json_agg(t) AS elements FROM (
+		  SELECT guid, name, cost, data
+		  FROM quota_details_agg
+		) t`,
+		string(since),
+		string(until),
+	).Scan(&quotas)
+	var data string
+	if err == nil {
+		data = fmt.Sprintf(`{"Quotas": %s}`, quotas)
+	} else {
 		fmt.Println(err)
+		data = `{"Quotas": []}`
 	}
-	fmt.Fprint(w, quotas)
+	fmt.Fprint(w, data)
 }
 
 func QuotaDetails(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 
+	//TODO: Convert to function
+	query_values := r.URL.Query()
+	since := query_values.Get("since")
+	if since == "" {
+		since = "1970-01-01"
+	}
+	until := query_values.Get("until")
+	if until == "" {
+		until = "2050-01-01"
+	}
+
+	//TODO: Setup to only open once
 	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
 	if err != nil {
 		fmt.Println(err)
 	}
 	defer db.Close()
 	var quotas string
-	err = db.QueryRow(
-		"SELECT details FROM quota_details WHERE guid = $1",
+	err = db.QueryRow(`
+		SELECT details
+		FROM get_quotas($1, $2)
+		WHERE guid = $3`,
+		since,
+		until,
 		ps.ByName("guid"),
 	).Scan(&quotas)
 	fmt.Fprint(w, strings.TrimRight(strings.TrimLeft(quotas, "["), "]"))
